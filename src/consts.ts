2d0995a3550a6344830e54ea0df62dda2c6628be
Jesse Black <jesse.w.black@gmail.com>
2026-04-17 16:07:01 -0500

    site title and description

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Jesse Black\'s Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Jesse Black';
+export const SITE_DESCRIPTION = 'Notes on agents and staying human.';
 export const AUTHOR_NAME = 'Jesse Black';
 export const GITHUB_PROFILE_URL = 'https://github.com/jesse-black';
 
