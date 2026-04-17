345750c124eb2d36ff182712418d30ffe420a54d
Jesse Black <jesse.w.black@gmail.com>
2026-04-17 15:36:10 -0500

    linking byline

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,8 @@
 
 export const SITE_TITLE = 'Jesse Black\'s Blog';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const AUTHOR_NAME = 'Jesse Black';
+export const GITHUB_PROFILE_URL = 'https://github.com/jesse-black';
 
 export const GITHUB_REPO = 'https://github.com/jesse-black/jesse-black.github.io';
 export const DISCUSSIONS_URL = `${GITHUB_REPO}/discussions`;
@@ -10,13 +12,13 @@ export const BLOG_SOURCE_URL = `${GITHUB_REPO}/blob/main/src/content/blog`;
 
 export const SOCIAL_LINKS: { href: string; label: string; icon: string }[] = [
 	{
-		href: 'https://github.com/jesse-black',
-		label: "Go to Jesse's GitHub",
+		href: GITHUB_PROFILE_URL,
+		label: `Go to ${AUTHOR_NAME}'s GitHub`,
 		icon: 'simple-icons:github',
 	},
 	{
 		href: 'https://instagram.com/jessewblack',
-		label: "Go to Jesse's Instagram",
+		label: `Go to ${AUTHOR_NAME}'s Instagram`,
 		icon: 'simple-icons:instagram',
 	},
 ];
