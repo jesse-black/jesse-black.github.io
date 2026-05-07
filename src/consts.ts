a35466a4d275322d17ed55506e040c6001eb4b7e
Jesse Black <jesse.w.black@gmail.com>
2026-05-07 18:29:52 -0500

    copy markdown button uses raw github source

--- src/consts.ts
+++ src/consts.ts
@@ -6,9 +6,11 @@ export const SITE_DESCRIPTION = 'Notes on agents and staying human.';
 export const AUTHOR_NAME = 'Jesse Black';
 export const GITHUB_PROFILE_URL = 'https://github.com/jesse-black';
 
-export const GITHUB_REPO = 'https://github.com/jesse-black/jesse-black.github.io';
-export const DISCUSSIONS_URL = `${GITHUB_REPO}/discussions`;
-export const BLOG_SOURCE_URL = `${GITHUB_REPO}/blob/main/src/content/blog`;
+export const GITHUB_REPO = 'jesse-black/jesse-black.github.io';
+export const GITHUB_REPO_URL = `https://github.com/${GITHUB_REPO}`;
+export const DISCUSSIONS_URL = `${GITHUB_REPO_URL}/discussions`;
+export const BLOG_SOURCE_URL = `${GITHUB_REPO_URL}/blob/main/src/content/blog`;
+export const BLOG_RAW_SOURCE_URL = `https://raw.githubusercontent.com/${GITHUB_REPO}/main/src/content/blog`;
 
 export const SOCIAL_LINKS: { href: string; label: string; icon: string }[] = [
 	{
