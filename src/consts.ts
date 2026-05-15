e80ff857faa31e8826b6252cc1c44b168988daee
Jesse Black <jesse.w.black@gmail.com>
2026-05-15 11:19:17 -0500

    add giscus

--- src/consts.ts
+++ src/consts.ts
@@ -12,6 +12,10 @@ export const DISCUSSIONS_URL = `${GITHUB_REPO_URL}/discussions`;
 export const BLOG_SOURCE_URL = `${GITHUB_REPO_URL}/blob/main/src/content/blog`;
 export const BLOG_RAW_SOURCE_URL = `https://raw.githubusercontent.com/${GITHUB_REPO}/main/src/content/blog`;
 
+export const GISCUS_REPO_ID = 'MDEwOlJlcG9zaXRvcnkxNTU0MzU1OTI=';
+export const GISCUS_CATEGORY = 'General';
+export const GISCUS_CATEGORY_ID = 'DIC_kwDOCUPCSM4C6xDy';
+
 export const SOCIAL_LINKS: { href: string; label: string; icon: string }[] = [
 	{
 		href: GITHUB_PROFILE_URL,
