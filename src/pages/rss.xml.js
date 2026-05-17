f4063778eb60e2a2bb88194756e83c4e91097476
Jesse Black <jesse.w.black@gmail.com>
2026-05-17 12:01:06 -0500

    add content to rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,6 +1,10 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+
+const parser = new MarkdownIt();
 
 export async function GET(context) {
 	const posts = await getCollection('blog');
@@ -11,6 +15,9 @@ export async function GET(context) {
 		items: posts.map((post) => ({
 			...post.data,
 			link: `/blog/${post.id}/`,
+			content: sanitizeHtml(parser.render(post.body), {
+				allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+			}),
 		})),
 	});
 }
